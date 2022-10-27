338e0ebf1b80b65ec2b6cc27f0dad6750b3112d1
Constance Wang <constancezwang@gmail.com>
2022-10-27 11:47:27 -0400

    add skeleton shape component

--- Source/Project5/Shape.h
+++ Source/Project5/Shape.h
@@ -6,6 +6,12 @@
 #include "Components/ActorComponent.h"
 #include "Shape.generated.h"
 
+UENUM()
+enum ShapeType {
+	Cylinder UMETA(DisplayName = "Cylinder"),
+	Cone UMETA(DisplayName = "Cone"),
+	Prism UMETA(DisplayName = "Prism"),
+};
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class PROJECT5_API UShape : public UActorComponent
@@ -14,6 +20,10 @@ class PROJECT5_API UShape : public UActorComponent
 
 public:	
 	// Sets default values for this component's properties
+	ShapeType shapeType;
+	FVector color;
+	FTransform transform;
+
 	UShape();
 
 protected:
